26e869dc6f4d6fc3d78f4a51edb89e27df77bc3a
Nicolás <nicolasbusch5@gmail.com>
2026-07-26 09:46:44 -0500

    Modificación para no descargar archivos repetidos

--- descarga.py
+++ descarga.py
@@ -2,16 +2,24 @@ import os
 import requests
 
 url_base = "https://physionet.org/files/pulse-transit-time-ppg/1.1.0/csv/"
-os.makedirs("./datos_csv", exist_ok=True)
+carpeta_destino = "./datos_csv"
+os.makedirs(carpeta_destino, exist_ok=True)
 
 archivos = [f"s{i}_{a}.csv" for i in range(1, 23) for a in ["sit", "walk", "run"]] + ["subjects_info.csv"]
 
 for i, nom in enumerate(archivos, 1):
-    print(f"[{i}/67] Descargando {nom}...", flush=True)
+    ruta_archivo = f"{carpeta_destino}/{nom}"
+    
+    # Comprueba si el archivo ya existe en la carpeta
+    if os.path.exists(ruta_archivo):
+        print(f"[{i}/{len(archivos)}] {nom} ya existe. Omitiendo...", flush=True)
+        continue  # Salta a la siguiente iteración del ciclo
+
+    print(f"[{i}/{len(archivos)}] Descargando {nom}...", flush=True)
     res = requests.get(url_base + nom, stream=True)
     
     if res.status_code == 200:
-        with open(f"./datos_csv/{nom}", "wb") as f:
+        with open(ruta_archivo, "wb") as f:
             descargado = 0
             # Lee y escribe en bloques de 1 MB
             for bloque in res.iter_content(chunk_size=1024 * 1024):
